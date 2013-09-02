4136333db20663f991ac693c667d74199d8aafd3
hmuronaka <caprice29@i.softbank.jp>
2013-09-03 02:05:40 +0900

    codegenlib追加

--- app/controllers/code_generators_controller.rb
+++ app/controllers/code_generators_controller.rb
@@ -1,11 +1,28 @@
+require 'CppGenerator.rb'
+require 'HeaderGenerator.rb'
+
 class CodeGeneratorsController < ApplicationController
   def generate
-    unless params[:res] 
-      @res = {type: :yaml,
-       from_code: "" 
-      }
-    else
-      @res = params[:res]
+    @res = {type: :yaml,
+     from_code: "" 
+    }
+
+    self_path = File.dirname(File.expand_path(__FILE__))
+
+    @res[:from_code] = File.read((Pathname.new(self_path) + './def.ini.yml').to_s)
+
+    if params[:res] 
+      @res[:type] = params[:res][:type]
+      @res[:from_code] = params[:res][:from_code]
+      #gen = CodegenLib::CppGenerator.new
+      gen = CodegenLib::HeaderGenerator.new
+      logger.info params[:res][:from_code]
+      yaml = YAML.load(params[:res][:from_code].gsub(/\r\n/, "\n" ))
+      @res[:to_code] = gen.generate("test", yaml)
+    end
+
+    respond_to do |format|
+      format.html
     end
   end
 end
